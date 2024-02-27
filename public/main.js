4268bf651e17468ae1fd7eb96bddd153784a9104
Mathias Magnusson <mathias@magnusson.space>
2024-02-27 15:21:53 +0100

    Make the animations nice

--- public/main.js
+++ public/main.js
@@ -4,8 +4,9 @@ const weekDay = document.querySelector("#weekday");
 const weekNr = document.querySelector("#week-nr");
 const date = document.querySelector("#date");
 const time = document.querySelector("#time");
-const sl = document.querySelector("iframe");
+const sl = document.querySelectorAll("iframe");
 
+let prevI = 0;
 setInterval(() => {
     const now = new Date();
     weekDay.innerText = now.toLocaleString("sv-SE", { timeZone: "Europe/Stockholm", weekday: "long" });
@@ -13,14 +14,17 @@ setInterval(() => {
     date.innerText = now.toLocaleString("sv-SE", { timeZone: "Europe/Stockholm", day: "numeric", month: "long", year: "numeric" });
     time.innerText = now.toLocaleString("sv-SE", { timeZone: "Europe/Stockholm", hour: "numeric", minute: "numeric", second: "numeric" });
 
-    const url = "https://sl.datasektionen.se";
-    const paths = ["/metro", "/tram", "/bus"];
-    const i = Math.floor(now.getSeconds() / 10) % paths.length;
-    const src = url + paths[i]
-    if (sl.src != src) {
+    const i = Math.floor(now.getSeconds() / 10) % sl.length;
+    function update() {
+        sl[prevI].style.visibility = "hidden";
+        sl[i].style.visibility = "visible";
+        prevI = i;
+    }
+    if (prevI != i) {
         if (document.startViewTransition)
-            document.startViewTransition(() => sl.src = src);
-        else
-            sl.src = src;
+            document.startViewTransition(update);
+        else {
+            update();
+        }
     }
 }, 1000);
